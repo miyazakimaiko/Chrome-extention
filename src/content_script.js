cc874af8a7035dbc50389287833303f155eb1639
miyazakimaiko <myzkmik19922@gmail.com>
2019-08-13 05:51:28 +0900

    Deleted unnecessary files

--- src/content_script.js
+++
@@ -1,5 +0,0 @@
-﻿document.getElementById('add-word-form').onsubmit = function() {
-    var contents = document.getElementById('add-word-form').contents.value;
-    chrome.strage.local.set({hold_content:contents})
-    console.log('Data is saved!')
-};
