12e3d7b5ec8c7e2db8c263d0864cc6d1c1739bde
miyazakimaiko <myzkmik19922@gmail.com>
2019-08-13 08:44:37 +0900

    Fixed: was using return false where it didnt need

--- src/save.js
+++ src/save.js
@@ -4,14 +4,12 @@ document.getElementById('add-tags').onsubmit = function(){
     
     var tags = document.getElementById('tag-input').value;
     
-    chrome.storage.local.set({tagData:tags});
-	return false; // Checked
+    chrome.storage.local.set({tagData:tags}); // Checked
 }
 
 document.getElementById('add-word-form').onsubmit = function(){
     
     var vocabulary = document.getElementById('vocabulary').value;
     
-    chrome.storage.local.set({vocab:vocabulary});
-	return false; // Checked
+    chrome.storage.local.set({vocab:vocabulary}); // Checked
 }
