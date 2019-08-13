5151f114e679f75e909aa2a0e2af2544e683f43b
miyazakimaiko <myzkmik19922@gmail.com>
2019-08-14 06:47:29 +0900

    Occuring error: can't create array

--- src/load.js
+++ src/load.js
@@ -1,31 +1,34 @@
 // They are still samples.
 
-chrome.storage.local.get("tagData", function(baseItems) {
+chrome.storage.local.get('tagData', function(baseItems) {
 
-    //This function is not working
+    //This function is working!
     //
+    console.log(baseItems.tagData);
 
     const tagContainer = document.getElementById('tag-container')
 
     const tagDiv = document.createElement('div')
 
-    const tagLabel = document.createElement('label').textContent = baseItems.tagData;
-    tagLabel.setAttribute('for', `${tagData}-input`)
-
     const tagInput = document.createElement('input')
-    tagInput.setAttribute('id', `${tagData}-input`)
+    tagInput.setAttribute('id', `${baseItems.tagData[1]}-input`)
     tagInput.setAttribute('type', 'checkbox')
 
-    tagContainer.appendChild(tagDiv)
-    tagDiv.appendChild(tagLabel)
-    tagDiv.appendChild(tagInput)
+    const tagLabel = document.createElement('label')
+    tagLabel.textContent = baseItems.tagData[1];
+    tagLabel.setAttribute('for', `${baseItems.tagData[1]}-input`)
 
+    tagContainer.appendChild(tagDiv)
+    tagDiv.appendChild(tagInput)
+    tagDiv.appendChild(tagLabel)
 
 });
 
-chrome.storage.local.get("vocab", function(items) {
+chrome.storage.local.get('vocab', function(items) {
     
     // This is working ok
+
+    console.log(items.vocab)
     //
     document.getElementById('main-word').textContent = items.vocab;
 });
--- src/save.js
+++ src/save.js
@@ -2,14 +2,33 @@
 
 document.getElementById('add-tags').onsubmit = function(){
     
-    var tags = document.getElementById('tag-input').value;
-    
-    chrome.storage.local.set({tagData:tags}); // Checked
+    chrome.storage.local.get({ tagData:[] }, function(result){
+
+        var tagData = result.tagData;
+
+        var newTag = document.getElementById('tag-input').value;  
+
+        tagData.push(newTag); // here has through an error
+
+        chrome.storage.local.set({ tagData:tagData });
+            
+    });
+
 }
+
+// Add words
+// this function only add word in vocab inseparately..
 
 document.getElementById('add-word-form').onsubmit = function(){
     
-    var vocabulary = document.getElementById('vocabulary').value;
+    chrome.storage.local.get('vocab', function({ vocab }){
+        
+        var newVocab = document.getElementById('vocabulary').value;
+
+        const nextVocab = vocab.concat(newVocab);
+
+        chrome.storage.local.set({vocab:nextVocab}); // Checksed
+    });
     
-    chrome.storage.local.set({vocab:vocabulary}); // Checked
+
 }
