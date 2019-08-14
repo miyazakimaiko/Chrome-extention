c4929181554b353537050fb31d66afe0ff9ea687
miyazakimaiko <myzkmik19922@gmail.com>
2019-08-15 06:26:16 +0900

    Add: tag & categ form dynamically appear on screen

--- src/load.js
+++ src/load.js
@@ -1,34 +1,71 @@
-// They are still samples.
+// Tag data
 
-chrome.storage.local.get('tagData', function(baseItems) {
+chrome.storage.local.get({ tagData:[] }, function(items) {
 
-    //This function is working!
-    //
-    console.log(baseItems.tagData);
+    let i;
+    for( i = 0; i < items.tagData.length; i++ ) {
 
     const tagContainer = document.getElementById('tag-container')
 
     const tagDiv = document.createElement('div')
 
     const tagInput = document.createElement('input')
-    tagInput.setAttribute('id', `${baseItems.tagData[1]}-input`)
+    tagInput.setAttribute('id', `tag${i}-input`)
     tagInput.setAttribute('type', 'checkbox')
 
     const tagLabel = document.createElement('label')
-    tagLabel.textContent = baseItems.tagData[1];
-    tagLabel.setAttribute('for', `${baseItems.tagData[1]}-input`)
+    tagLabel.textContent = items.tagData[i];
+    tagLabel.setAttribute('for', `tag${i}-input`)
 
     tagContainer.appendChild(tagDiv)
     tagDiv.appendChild(tagInput)
     tagDiv.appendChild(tagLabel)
+            
+    }
 
 });
 
-chrome.storage.local.get('vocab', function(items) {
-    
-    // This is working ok
+// Category data
+chrome.storage.local.get({ catData:[] }, function(items) {
 
-    console.log(items.vocab)
+    let i;
+    for( i = 0; i < items.catData.length; i++ ) {
+    // Build add-word-drawer
+    const catContainer = document.getElementById('category-container')
+
+    const catLabel = document.createElement('label')
+
+    const catInput = document.createElement('input')
+    catInput.setAttribute('type', 'radio')
+    catInput.setAttribute('name', 'category')
+
+    const catSpan = document.createElement('span')
+    catSpan.textContent = items.catData[i];
+
+    catContainer.appendChild(catLabel)
+    catLabel.appendChild(catInput)
+    catLabel.appendChild(catSpan)
+
+    // Build nav-drawer contents
+    const navContainer = document.getElementById('nav-cat-container')
+
+    const navDiv = document.createElement('div')
+
+    const navLink = document.createElement('a')
+    navLink.setAttribute('href', '#')
+    navLink.textContent = items.catData[i];
+
+    navContainer.appendChild(navDiv)
+    navDiv.appendChild(navLink)
+
+    }
+
+});
+
+
+chrome.storage.local.get({ words:[] }, function(items) {
     //
-    document.getElementById('main-word').textContent = items.vocab;
+    console.log(items.words);
+    document.getElementById('main-word').textContent = items.words;
+
 });
--- src/save.js
+++ src/save.js
@@ -1,34 +1,51 @@
 // Create tags
-
 document.getElementById('add-tags').onsubmit = function(){
     
-    chrome.storage.local.get({ tagData:[] }, function(result){
+    chrome.storage.local.get({ tagData:[] }, function(items) {
 
-        var tagData = result.tagData;
+        let newTag = document.getElementById('tag-input').value;
 
-        var newTag = document.getElementById('tag-input').value;  
+        items.tagData.push(newTag);
 
-        tagData.push(newTag); // here has through an error
+        chrome.storage.local.set(items);
 
-        chrome.storage.local.set({ tagData:tagData });
-            
     });
 
 }
 
-// Add words
-// this function only add word in vocab inseparately..
+// Create categories
+document.getElementById('add-category').onsubmit = function(){
+    
+    chrome.storage.local.get({ catData:[] }, function(items) {
 
+        let newCat = document.getElementById('category-input').value;
+
+        items.catData.push(newCat);
+
+        chrome.storage.local.set(items);
+
+    });
+
+}
+
+
+// Add words
+// Still working on this part--------------------------------------------
 document.getElementById('add-word-form').onsubmit = function(){
     
-    chrome.storage.local.get('vocab', function({ vocab }){
-        
-        var newVocab = document.getElementById('vocabulary').value;
+    chrome.storage.local.get({ wordInfo:[] }, function(items) {
 
-        const nextVocab = vocab.concat(newVocab);
+        let newWord = [];
 
-        chrome.storage.local.set({vocab:nextVocab}); // Checksed
+        let word = document.getElementById('vocabulary').value;
+
+        let tags = 
+        items.wordInfo.push({ wordInfo:newWord });
+
+        let newTags = 
+
+        chrome.storage.local.set(items);
+
     });
-    
 
 }
