51e12b77d7d87386eb00abbf5b1d6b6ea5727ac9
miyazakimaiko <myzkmik19922@gmail.com>
2019-08-20 23:35:52 +0100

    Deleted: window.onload func

--- src/save.js
+++ src/save.js
@@ -1,6 +1,6 @@
-window.onload = () => {
+// Create tags
+if ( document.getElementById('add-tags') ) {
 
-    // Create tags
     document.getElementById('add-tags').onsubmit = () => {
 
         let newTag = document.getElementById('tag-input').value;
@@ -24,9 +24,9 @@ window.onload = () => {
     
 }
 
-window.onload = () => {
+// Create categories
+if ( document.getElementById('add-category') ) {
 
-    // Create categories
     document.getElementById('add-category').onsubmit = () => {
         
         chrome.storage.local.get({ catData:[] }, function(items) {
@@ -50,86 +50,82 @@ window.onload = () => {
 
 }
 
-window.onload = () => {
+// Add words
+if ( document.getElementById('add-word-form') ) {
 
-    if ( document.getElementById('add-word-form') ) {
+    document.getElementById('add-word-form').onsubmit = () => {
 
-        // Add words
-        document.getElementById('add-word-form').onsubmit = () => {
+        chrome.storage.local.get(null, function(items) {
 
-            chrome.storage.local.get(null, function(items) {
-
-                // Create ID"
-                if (items.countId === undefined || items.countId === 0) {
-                    let id = 1
-                    items.countId = id;
-                    chrome.storage.local.set(items);
-                } else {
-                    items.countId++
-                    chrome.storage.local.set(items);
-                }
-
-                // init array
-                if(!items.wordInfo){
-                    items.wordInfo = [];
-                }
-                const newWord = { id:'', word:'', tag:[], category:'', meanings:'' };
-
-                // Add ID
-                const cId = items.countId;
-                newWord.id = cId;
-
-                // Push word
-                let vocab = document.getElementById('vocabulary').value;
-                newWord.word = vocab;
-
-                // Push meaning
-                let meaning = document.getElementById('meaning-textarea').value;
-                newWord.meanings = meaning;
-
-                // Push tag
-                let tagEl = document.getElementById('tag-container');
-
-                let tags = tagEl.getElementsByTagName('input');
-
-                for (let i=0, len=tags.length; i<len; i++ ) {
-                    
-                    if (tags[i].checked) {
-
-                        let checkedLabel = document.getElementById(`tag${i}-label`).textContent;
-                        newWord.tag.push(checkedLabel);
-
-                    }
-
-                }
-
-                // Push category
-                let catEl = document.getElementById('category-container');
-
-                let cat = catEl.getElementsByTagName('input');
-
-                for (let i=0, len=cat.length; i<len; i++ ) {
-                    
-                    if (cat[i].checked) {
-
-                        let checkedSpan = document.getElementById(`cat${i}-span`).textContent;
-                        newWord.category = checkedSpan;
-
-                    }
-
-                }
-
-                if(items.wordInfo === undefined ) {
-                    items.wordInfo[0] = newWord;
-                } else {
-                    items.wordInfo.unshift(newWord); 
-                }
-
+            // Create ID"
+            if (items.countId === undefined || items.countId === 0) {
+                let id = 1
+                items.countId = id;
                 chrome.storage.local.set(items);
+            } else {
+                items.countId++
+                chrome.storage.local.set(items);
+            }
 
-            });
+            // init array
+            if(!items.wordInfo){
+                items.wordInfo = [];
+            }
+            const newWord = { id:'', word:'', tag:[], category:'', meanings:'' };
 
-        }
+            // Add ID
+            const cId = items.countId;
+            newWord.id = cId;
+
+            // Push word
+            let vocab = document.getElementById('vocabulary').value;
+            newWord.word = vocab;
+
+            // Push meaning
+            let meaning = document.getElementById('meaning-textarea').value;
+            newWord.meanings = meaning;
+
+            // Push tag
+            let tagEl = document.getElementById('tag-container');
+
+            let tags = tagEl.getElementsByTagName('input');
+
+            for (let i=0, len=tags.length; i<len; i++ ) {
+                
+                if (tags[i].checked) {
+
+                    let checkedLabel = document.getElementById(`tag${i}-label`).textContent;
+                    newWord.tag.push(checkedLabel);
+
+                }
+
+            }
+
+            // Push category
+            let catEl = document.getElementById('category-container');
+
+            let cat = catEl.getElementsByTagName('input');
+
+            for (let i=0, len=cat.length; i<len; i++ ) {
+                
+                if (cat[i].checked) {
+
+                    let checkedSpan = document.getElementById(`cat${i}-span`).textContent;
+                    newWord.category = checkedSpan;
+
+                }
+
+            }
+
+            if(items.wordInfo === undefined ) {
+                items.wordInfo[0] = newWord;
+            } else {
+                items.wordInfo.unshift(newWord); 
+            }
+
+            chrome.storage.local.set(items);
+
+        });
 
     }
 
