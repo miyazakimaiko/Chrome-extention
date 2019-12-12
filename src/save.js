b48289106a2572a2740e31104b73c2e0b47a355e
miyazakimaiko <myzkmik19922@gmail.com>
2019-12-12 02:38:24 +0000

    Changed alert when submitting forms

--- src/save.js
+++ src/save.js
@@ -4,48 +4,71 @@ if ( document.getElementById('add-tags') ) {
     document.getElementById('add-tags').onsubmit = () => {
 
         let newTag = document.getElementById('tag-input').value;
+        const alert = document.getElementById("alert-success-t")
 
         chrome.storage.local.get({ tagData:[] }, (items) => {
 
-            if ( newTag === '' ) {
-                alert('Please fill the form.');
-                return false;
+            if ( !newTag.length ) {
+                alert.classList.add('alert-danger')
+                alert.innerHTML = "Please fill the form.";
             }
-            if (items.tagData.indexOf(newTag) === -1 ) {
+            else if (items.tagData.indexOf(newTag) === -1 ) {
                 items.tagData.push(newTag);
                 chrome.storage.local.set(items);
+                alert.classList.add('alert-success')
+                alert.innerHTML = `Successfully added a tag [${newTag}].`;
             } else {
-                alert('This tag already exists.');
+                alert.classList.add('alert-danger')
+                alert.innerHTML = `Tag[${newTag}] already exists.`;
             }
+            showAlert(alert);
 
         });
-
+        return false;
     }
-    
+
 }
+
+const showAlert = (alert) => {
+    alert.style.display = "flex";
+    setTimeout(function() { 
+        clearAlert(alert) 
+    }, 3000);
+}
+
+const clearAlert = (alert) => {
+    alert.style.display = "none";
+    alert.classList.remove('alert-danger')
+}
+
 
 // Create categories
 if ( document.getElementById('add-category') ) {
 
     document.getElementById('add-category').onsubmit = () => {
+
+        let newCat = document.getElementById('category-input').value;
+        const alert = document.getElementById("alert-success-c")
         
         chrome.storage.local.get({ catData:[] }, (items) => {
-
-            let newCat = document.getElementById('category-input').value;
             
-            if ( newCat === '' ) {
-                alert('Please fill the form.')
-                return false
+            if ( !newCat.length ) {
+                alert.classList.add('alert-danger')
+                alert.innerHTML = "Please fill the form.";
             }
-            if (items.catData.indexOf(newCat) === -1 ) {
+            else if (items.catData.indexOf(newCat) === -1 ) {
                 items.catData.push(newCat);
                 chrome.storage.local.set(items);
+                alert.classList.add('alert-success')
+                alert.innerHTML = `Successfully added a category [${newCat}].`;
             } else {
-                alert('This category already exist.')
+                alert.classList.add('alert-danger')
+                alert.innerHTML = `Tag[${newCat}] already exists.`;
             }
+            showAlert(alert);
 
         });
-
+        return false;
     }
 
 }
